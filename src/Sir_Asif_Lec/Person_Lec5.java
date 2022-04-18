b6b7178eaf5c9402f01689866b9b2b5715614444
Nasratredi1 <73361214+Nasratredi1@users.noreply.github.com>
2022-04-18 23:18:08 +0500

    Lecture 5

---
+++ src/Sir_Asif_Lec/Person_Lec5.java
@@ -0,0 +1,10 @@
+package Sir_Asif_Lec;
+
+public class Person_Lec5 {
+    int id;
+    String name;
+    int age;
+    String email;
+    String address;
+    String mobile;
+}
---
+++ src/Sir_Asif_Lec/TestApp_5.java
@@ -0,0 +1,53 @@
+package Sir_Asif_Lec;
+// lec No: 5
+public class TestApp_5 {
+    public static void main (String args[]){
+       int a = 20;
+
+       // p1 store the reference
+       Person_Lec5 p1 = new Person_Lec5();
+
+       p1.id = 1;
+       p1.name = "Nasrat";
+       p1.age = 22;
+       p1.mobile = "03433435";
+
+       Person_Lec5 p2 = new Person_Lec5();
+       p2.id = 2;
+
+       printData(p1);
+
+    }
+
+    public static void printData (Person_Lec5 p){
+        System.out.println(p.id);
+        System.out.println(p.name);
+        System.out.println(p.age);
+    }
+
+
+}
+
+
+/*
+
+To remove this ambiguity we need to declare class
+ // for One object we need more attribute like person
+        int personId;
+        String name;
+        int age;
+        String email;
+
+        // for book
+        int bookId;
+        String title;
+        int year;
+
+        LEC no: 6
+        Real life object also have behaviour
+        they perform some activity.
+        like student :  course register, task performed, take class
+
+        Behaviour mean: object perform some action or activity.
+ */
+
